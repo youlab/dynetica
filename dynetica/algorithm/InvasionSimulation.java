65235fdc86b6072830fadc71b46b852c68f3c645
Billy Xizheng Wan <xw58@duke.edu>
2016-02-16 00:48:32 -0500

    initial commit for invasion simulation

---
+++ dynetica/algorithm/InvasionSimulation.java
@@ -0,0 +1,16 @@
+package dynetica.algorithm;
+
+/**
+ *
+ * @author Billy Wan
+ */
+
+import dynetica.entity.*;
+import dynetica.system.*;
+
+public class InvasionSimulation implements Runnable {
+    
+    public void run() {
+        
+    }
+}
